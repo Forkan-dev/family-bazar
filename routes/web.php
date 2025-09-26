6fbcfd618b334f5ce2d76244769a121bc9d4b714
Forkan Alam <forkanalam295@gmail.com>
2025-09-26 23:30:34 +0600

    home route with login fixed

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,3 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
-
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
-// require __DIR__.'/admin.php';
