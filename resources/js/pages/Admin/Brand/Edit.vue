960d8214880d2c1cce8070d6db8f8539cc620268
SyeedSharek <syeedsharek1995@gmail.com>
2025-10-29 21:46:01 +0600

    test

--- resources/js/pages/Admin/Brand/Edit.vue
+++ resources/js/pages/Admin/Brand/Edit.vue
@@ -51,7 +51,7 @@ const submit = () => {
                         <v-divider></v-divider>
                         <v-card-text>
                             <v-form @submit.prevent="submit">
-                                <v-row>
+                                <v-row class="mt-4">
                                     <v-col cols="12" md="6">
                                         <VInputField
                                             v-model="form.en_name"
@@ -81,7 +81,7 @@ const submit = () => {
                                             variant="outlined"
                                         />
                                     </v-col>
-                                    <v-col cols="12" md="6">
+                                    <v-col cols="12">
                                         <VFileInput
                                             v-model="form.image"
                                             title="Upload New Image"
